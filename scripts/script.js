a69cf6dd62c33a1259f62226bd2350a870d16cc3
Suyeon Lee <slee1055@my.bcit.ca>
2023-04-18 23:01:50 -0700

    Last correction

--- scripts/script.js
+++ scripts/script.js
@@ -1,71 +1,93 @@
-const search = document.getElementById('search');
-search.addEventListener("click", function() {
-    const input = document.getElementById('input');
-    // The input should be cleared after search button is pressed
-    input.value = "";
-    // If the value entered in the input is not "Book", show an alert to the user
-    if (input.value !== "Book") {
-        alert("Please enter a valid search term");
+document.addEventListener('DOMContentLoaded', () => {
+  const searchButton = document.getElementById('search');
+  const inputField = document.getElementById('input');
+  const booksSection = document.querySelector('.books');
+  const wishListSection = document.querySelector('.wish-list');
+  const wishList = [];
+
+  function updateWishList(name, imagePath) {
+    // If the item is already in the wish list, show an alert and return
+    if (wishList.some((item) => item.name === name)) {
+      alert('This item has already been added to your wish list.');
+      return;
     }
 
-    if (input.value === "Book") {
-        // Make the hidden items visible ( Remember that you added a CSS rule to make some items hidden - use classList)
-        const hiddenItems = document.getElementsByClassName('box');
-        hiddenItems.classList.remove('hidden');
-    }
+    const itemDiv = document.createElement('div');
+    itemDiv.classList.add('wish-list-item');
 
-    // In a loop, create and append 4 image and span elements to the first section.
-    const section1 = document.querySelector('.book');
-    console.log(section1)
-    // Use template literal for images' paths
     const img = document.createElement('img');
+    img.src = imagePath;
+    img.alt = name;
+    img.width = 100;
+    img.height = 100;
+
     const span = document.createElement('span');
-    section1.appendChild(img);
-    section1.appendChild(span);
-    for(let i = 1; i <= 4; i++){
-        img.src = `Images/Book${i}.png`;
-        img.alt = `Book${i}`;
-        span.textContent = `Book${i}`;
-    }
-    document.body.appendChild(section1);
+    span.textContent = name;
+
+    itemDiv.appendChild(img);
+    itemDiv.appendChild(span);
+    wishListSection.appendChild(itemDiv);
     
+    // Add the item to the wish list array
+    wishList.push({ name: name });
+
+    // Remove the last-item class from the previous item
+    const lastItem = wishListSection.querySelector('.last-item');
+    if (lastItem) {
+      lastItem.classList.remove('last-item');
+      lastItem.style.border = 'none';
+    }
+    // Add the last-item class to the current item
+    itemDiv.classList.add('last-item');
+    itemDiv.style.borderBottom = '3px solid purple';
+  }
+
+// add event listener to the books section
+ booksSection.addEventListener('click', (event) => {
+    if (event.target.tagName === 'IMG') {
+      // Get the name and image path of the clicked item
+      const name = event.target.id;
+      const imagePath = event.target.src;
+      // update the wish list
+      updateWishList(name, imagePath);
+    }
+  });
+// add event listener to the search button
+searchButton.addEventListener('click', () => {
+    const searchText = inputField.value.trim();
+    // Clear the input field
+    inputField.value = '';
+
+    // If the search text is not "Book", show an alert
+    if (searchText !== 'Book') {
+      alert('Please search for "Book"');
+      return;
+    }
+
+    // Remove the hidden class from the books section
+    const hiddenItems = document.getElementsByClassName('hidden');
+    for (const item of hiddenItems) {
+      item.classList.remove('hidden');
+    }
+
+    // Create books
+    for (let i = 1; i <= 4; i++) {
+      const bookDiv = document.createElement('div');
+      bookDiv.classList.add('book-item');
+
+      const img = document.createElement('img');
+      img.src = `Images/Book${i}.png`;
+      img.alt = `Book${i}`;
+      img.id = `Book${i}`;
+
+      const span = document.createElement('span');
+      span.textContent = `Book${i}`;
+
+      bookDiv.appendChild(img);
+      bookDiv.appendChild(span);
+
+      booksSection.appendChild(bookDiv);
+    }
+  });
 });
-
-const books = [];
-const section1 = document.querySelector('.book');
-section1.addEventListener('click', function(e){
-    if(e.target.tagName === 'IMG'){
-        books.push(e.target.alt);
-        const span = e.target.nextElementSibling;
-        span.style.display = 'block';
-    }
-
-    const wishList = document.querySelector('.wishlist');
-    const ul = document.createElement('ul');
-    const li = document.createElement('li');
-    for(let i = 0; i < books.length; i++){
-        li.textContent = books[i];
-        const span = document.createElement('span');
-        span.textContent = books[i];
-        li.appendChild(span);
-        const img = document.createElement('img');
-        img.src = `Images/${books[i]}.png`;
-        img.alt = books[i];
-        img.width = 100;
-        img.height = 100;
-        li.appendChild(img);
-    } 
-    ul.appendChild(li);
-    wishList.appendChild(ul);
-    document.body.appendChild(wishList);
-    const booklist = {
-        name: books[i]
-    }
-    books.push(booklist);
-    if(books.length > 1){
-        alert('This item has already been added to your wish list');
-    }
-    const lastLi = ul.lastElementChild;
-    lastLi.style.border = '1px solid black';
-});    
 
